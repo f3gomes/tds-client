d93045985ecf024b6e0b4851f14ab7b1f9712d28
f3gomes <fgomesdeluna@gmail.com>
2025-01-03 10:44:02 -0300

    feat: include socket and global styles

---
+++ socket.ts
@@ -0,0 +1,24 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import { ClientSocket } from "./src/types/event.types";
+
+export const socketUrl = "https://tds-server.onrender.com";
+
+export const socket: ClientSocket = io(socketUrl);
+
+export const SocketContext = createContext(socket);
+
+export function useSocket() {
+  const [hasConnected, setHasConnected] = useState(false);
+  const socket = useContext(SocketContext);
+
+  // rerender when connected, to allow access to id
+  useEffect(() => {
+    !hasConnected &&
+      socket.on("connect", () => {
+        setHasConnected(true);
+      });
+  });
+
+  return socket;
+}
