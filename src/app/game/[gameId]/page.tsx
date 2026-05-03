9effb8be733b26313c5b3b3bbf0d8235246f17c1
f3gomes <fgomesdeluna@gmail.com>
2026-05-02 22:13:39 -0300

    fix: ts build error

--- src/app/game/[gameId]/page.tsx
+++ src/app/game/[gameId]/page.tsx
@@ -97,7 +97,7 @@ export default function Game({
                 }, 4000);
               }
             }}
-            onFlipComplete={(card): void => {
+            onFlipComplete={(_card): void => {
               // switch (card.type) {
               //   case CardType.EMPTY:
               //     return sounds.playRevealEmptySound();
